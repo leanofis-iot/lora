af8dff9db445d06dd3b65071e365d0aa621188e8
emitterpark <42601356+emitterpark@users.noreply.github.com>
2019-12-30 17:02:22 +0300

    do

---
+++ rtu-old/arduino/config/config.ino
@@ -0,0 +1,22 @@
+#include <EEPROM.h>
+
+struct Conf {
+  const uint16_t read_p = 1;
+  const uint16_t send_p = 30;
+  const float alr_max[2] = {55, 55};
+  const float alr_min[2] = {10, 10};
+  const float alr_hys[2] = {5, 5};
+  const float cal_b[2] = {-8.4, -8.4};  
+  const uint8_t an_type[2] = {1, 0};
+  const uint8_t dig_type[2] = {0, 0}; 
+  const uint8_t dr = 2; 
+};
+
+Conf conf;
+
+void setup() {
+  EEPROM.put(0, conf);
+}
+
+void loop() {
+}
