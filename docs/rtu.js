13eb412399e060009941a240c97476609a38e602
emitterpark <42601356+emitterpark@users.noreply.github.com>
2020-01-27 15:58:16 +0300

    do

--- docs/rtu.js
+++ docs/rtu.js
@@ -144,3 +144,74 @@
     });
   });
 })();
+
+/*
+saveButton.addEventListener('click', function() {
+  //if (!port) {
+  //  return;
+  //}
+  let config = '';      
+  if (configurationForm.checkValidity()) {
+    statusDisplay.textContent = 'heyyy';
+    for (let i = 0; i < 2; i++) {
+      let ii = '&lrb';        
+      ii += ('0' + i).slice (-2);      
+      let lrb = parseInt(document.getElementById(ii).value);
+      config += ii + lrb + '\r\n';
+    }
+    for (let i = 0; i < 1; i++) {
+      let ii = '&lrw';        
+      ii += ('0' + i).slice (-2);      
+      let lrw = parseInt(document.getElementById(ii).value);
+      config += ii + lrw + '\r\n';
+    }
+    let tmb = 0;                  
+    for (let i = 0; i < 10; i++) {
+      let ii = '&tmb';
+      ii += ('0' + i).slice (-2);
+      if (document.getElementById(ii).type === 'checkbox') { // if id ii = trg-upl
+        tmb = document.getElementById(ii).checked ? 1 : 0;            
+      } else {
+        tmb = parseInt(document.getElementById(ii).value);
+      }           
+      config += ii + tmb + '\r\n';
+    }
+    for (let i = 0; i < 2; i++) {
+      let ii = '&rlw';        
+      ii += ('0' + i).slice (-2);      
+      let rlw = parseInt(document.getElementById(ii).value);
+      config += ii + rlw + '\r\n';
+    }
+    let anb = 0;                  
+    for (let i = 0; i < 26; i++) {
+      let ii = '&anb';
+      ii += ('0' + i).slice (-2);
+      if (document.getElementById(ii).type === 'checkbox') {
+        anb = document.getElementById(ii).checked ? 1 : 0;            
+      } else {
+        anb = parseInt(document.getElementById(ii).value);
+      }           
+      config += ii + anb + '\r\n';
+    }
+    for (let i = 0; i < 16; i++) {
+      let ii = '&anf';        
+      ii += ('0' + i).slice (-2);      
+      let anf = parseFloat(document.getElementById(ii).value);
+      config += ii + anf + '\r\n';
+    }
+    let dgb = 0;                  
+    for (let i = 0; i < 12; i++) {
+      let ii = '&dgb';
+      ii += ('0' + i).slice (-2);
+      if (document.getElementById(ii).type === 'checkbox') {
+        dgb = document.getElementById(ii).checked ? 1 : 0;            
+      } else {
+        dgb = parseInt(document.getElementById(ii).value);
+      }           
+      config += ii + dgb + '\r\n';
+    }        
+  }      
+  statusDisplay.textContent = config;      
+  //port.send(config);      
+});
+*/
