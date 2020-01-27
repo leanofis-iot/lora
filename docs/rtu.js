58065d8b3118e34753a854fd846b052ce761c6b6
emitterpark <42601356+emitterpark@users.noreply.github.com>
2020-01-27 20:14:52 +0300

    do

--- docs/rtu.js
+++ docs/rtu.js
@@ -32,14 +32,13 @@
     trgAddButton.addEventListener('click', function() {
       let temp = document.getElementsByTagName("template")[0]; 
       let clon = temp.content.cloneNode(true);     
-      trgList.appendChild(clon);
-      
+      trgList.appendChild(clon);      
       statusDisplay.textContent = '';
       let lis = trgList.querySelectorAll('li');      
       for (let i = 0; i < lis.length; i++) {
         let els = lis[i].querySelectorAll('input,select');
         for (let j = 0; j < els.length; j++) {
-          els[j].id = 'alr_b' + ('0' +  String(j + i * 6)).slice (-2);
+          els[j].id = '&alr_b' + ('0' +  String(j + i * 6)).slice (-2);
           statusDisplay.textContent += els[j].id;
         }
       }
@@ -55,75 +54,24 @@
       let config = '';      
       if (configurationForm.checkValidity()) {
         statusDisplay.textContent = 'validaion ok';
-
-        let els = document.body.querySelectorAll('input,select');
+        let els = configurationForm.querySelectorAll('input,select');
         for (let i = 0; i < els.length; i++) {
           if (els[i].id[0] == '&') {
-            if (els[i].id[5] == 'b') {              
+            let val = 0;
+            if (els[i].id[5] == 'b') { 
+              if (els[i].type === 'checkbox') {
+                val = els[i].checked ? 1 : 0;            
+              } else {
+                val = parseInt(els[i].value);
+              }  
             } else if (els[i].id[5] == 'w') {
+              val = parseInt(els[i].value);
             } else if (els[i].id[5] == 'f') {
+              val = parseFloat(els[i].value);
             }
-          }          
-        }
-
-
-        for (let i = 0; i < 2; i++) {
-          let ii = '&lrb';        
-          ii += ('0' + i).slice (-2);      
-          let lrb = parseInt(document.getElementById(ii).value);
-          config += ii + lrb + '\r\n';
-        }
-        for (let i = 0; i < 1; i++) {
-          let ii = '&lrw';        
-          ii += ('0' + i).slice (-2);      
-          let lrw = parseInt(document.getElementById(ii).value);
-          config += ii + lrw + '\r\n';
-        }
-        let tmb = 0;                  
-        for (let i = 0; i < 10; i++) {
-          let ii = '&tmb';
-          ii += ('0' + i).slice (-2);
-          if (document.getElementById(ii).type === 'checkbox') { // if id ii = trg-upl
-            tmb = document.getElementById(ii).checked ? 1 : 0;            
-          } else {
-            tmb = parseInt(document.getElementById(ii).value);
-          }           
-          config += ii + tmb + '\r\n';
-        }
-        for (let i = 0; i < 2; i++) {
-          let ii = '&rlw';        
-          ii += ('0' + i).slice (-2);      
-          let rlw = parseInt(document.getElementById(ii).value);
-          config += ii + rlw + '\r\n';
-        }
-        let anb = 0;                  
-        for (let i = 0; i < 26; i++) {
-          let ii = '&anb';
-          ii += ('0' + i).slice (-2);
-          if (document.getElementById(ii).type === 'checkbox') {
-            anb = document.getElementById(ii).checked ? 1 : 0;            
-          } else {
-            anb = parseInt(document.getElementById(ii).value);
-          }           
-          config += ii + anb + '\r\n';
-        }
-        for (let i = 0; i < 16; i++) {
-          let ii = '&anf';        
-          ii += ('0' + i).slice (-2);      
-          let anf = parseFloat(document.getElementById(ii).value);
-          config += ii + anf + '\r\n';
-        }
-        let dgb = 0;                  
-        for (let i = 0; i < 12; i++) {
-          let ii = '&dgb';
-          ii += ('0' + i).slice (-2);
-          if (document.getElementById(ii).type === 'checkbox') {
-            dgb = document.getElementById(ii).checked ? 1 : 0;            
-          } else {
-            dgb = parseInt(document.getElementById(ii).value);
-          }           
-          config += ii + dgb + '\r\n';
-        }        
+            config += els[i].id + val + '\r\n';
+          }                     
+        }                  
       }      
       statusDisplay.textContent = config;      
       //port.send(config);      
